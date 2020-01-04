794560c9c18babfc2897e14967f8f33988aa9440
Ilkin Bayramli <ibayramli@holylogin01.rc.fas.harvard.edu>
2020-01-04 07:51:12 -0500

    Change train_gpu parameters

--- multi3net/src/train_gpu.sh
+++ multi3net/src/train_gpu.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-#SBATCH --gres=gpu  
+#SBATCH -N 1
+#SBATCH --gres=gpu:2
+#SBATCH -t 0-01:00        
 #SBATCH --gpu-freq=high
-#SBATCH -t 0-30:00        
-#SBATCH -p gpu            
-#SBATCH --mem=40000        
+#SBATCH -p gpu_test       
+#SBATCH --mem-per-gpu=30000
 #SBATCH -o myoutput_prepost_0.01_0.9_8%j.out 
 #SBATCH -e myerrors_prepost_0.01_0.9_8%j.err
 
-python train.py -x 'vhr_post_pre' -o '/n/tambe_lab/disaster_relief/multi3net/results/predictions_pre_post' -e 20 -b 8 
+python train.py -x 'vhr_post_pre' -o '/n/tambe_lab/disaster_relief/multi3net/results/predictions_pre_post' -e 20 -b 1
