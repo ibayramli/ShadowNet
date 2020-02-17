a3740b32834a46f7b88c52dbf8f1d2f20ec72be0
Ilkin Bayramli <ibayramli2001@gmail.com>
2020-02-16 21:04:07 -0500

    Extend Siamese models

--- multi3net/src/models/siamese_models.py
+++ multi3net/src/models/siamese_models.py
@@ -11,341 +11,106 @@ from utils.trainer import tensor_to_variable
 class SiamUnet_conc(nn.Module):
     """SiamUnet_conc segmentation network."""
 
-    def __init__(self, input_nbr, label_nbr):
+    def __init__(self, n_channels, n_classes):
         super(SiamUnet_conc, self).__init__()
+	
+        self.n_classes = n_classes
+        self.inc = inconv(n_channels, 64)
+        self.down1 = down(64, 128)
+        self.down2 = down(128, 256)
+        self.down3 = down(256, 512)
+        self.down4 = down(512, 512)
+        self.up1 = up_skip_conc(1024, 256)
+        self.up2 = up_skip_conc(512, 128)
+        self.up3 = up_skip_conc(256, 64)
+        self.up4 = up_skip_conc(128, 64)
+        self.outc = outconv(64, n_classes)
 
-        self.input_nbr = input_nbr
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
-        self.conv11 = nn.Conv2d(input_nbr, 16, kernel_size=3, padding=1)
-        self.bn11 = nn.BatchNorm2d(16)
-        self.do11 = nn.Dropout2d(p=0.2)
-        self.conv12 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
-        self.bn12 = nn.BatchNorm2d(16)
-        self.do12 = nn.Dropout2d(p=0.2)
-
-        self.conv21 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.bn21 = nn.BatchNorm2d(32)
-        self.do21 = nn.Dropout2d(p=0.2)
-        self.conv22 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
-        self.bn22 = nn.BatchNorm2d(32)
-        self.do22 = nn.Dropout2d(p=0.2)
-
-        self.conv31 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.bn31 = nn.BatchNorm2d(64)
-        self.do31 = nn.Dropout2d(p=0.2)
-        self.conv32 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.bn32 = nn.BatchNorm2d(64)
-        self.do32 = nn.Dropout2d(p=0.2)
-        self.conv33 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.bn33 = nn.BatchNorm2d(64)
-        self.do33 = nn.Dropout2d(p=0.2)
-
-        self.conv41 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.bn41 = nn.BatchNorm2d(128)
-        self.do41 = nn.Dropout2d(p=0.2)
-        self.conv42 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.bn42 = nn.BatchNorm2d(128)
-        self.do42 = nn.Dropout2d(p=0.2)
-        self.conv43 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.bn43 = nn.BatchNorm2d(128)
-        self.do43 = nn.Dropout2d(p=0.2)
-
-        self.upconv4 = nn.ConvTranspose2d(128, 128, kernel_size=3, padding=1, stride=2, output_padding=1)
-
-        self.conv43d = nn.ConvTranspose2d(384, 128, kernel_size=3, padding=1)
-        self.bn43d = nn.BatchNorm2d(128)
-        self.do43d = nn.Dropout2d(p=0.2)
-        self.conv42d = nn.ConvTranspose2d(128, 128, kernel_size=3, padding=1)
-        self.bn42d = nn.BatchNorm2d(128)
-        self.do42d = nn.Dropout2d(p=0.2)
-        self.conv41d = nn.ConvTranspose2d(128, 64, kernel_size=3, padding=1)
-        self.bn41d = nn.BatchNorm2d(64)
-        self.do41d = nn.Dropout2d(p=0.2)
-
-        self.upconv3 = nn.ConvTranspose2d(64, 64, kernel_size=3, padding=1, stride=2, output_padding=1)
-
-        self.conv33d = nn.ConvTranspose2d(192, 64, kernel_size=3, padding=1)
-        self.bn33d = nn.BatchNorm2d(64)
-        self.do33d = nn.Dropout2d(p=0.2)
-        self.conv32d = nn.ConvTranspose2d(64, 64, kernel_size=3, padding=1)
-        self.bn32d = nn.BatchNorm2d(64)
-        self.do32d = nn.Dropout2d(p=0.2)
-        self.conv31d = nn.ConvTranspose2d(64, 32, kernel_size=3, padding=1)
-        self.bn31d = nn.BatchNorm2d(32)
-        self.do31d = nn.Dropout2d(p=0.2)
-
-        self.upconv2 = nn.ConvTranspose2d(32, 32, kernel_size=3, padding=1, stride=2, output_padding=1)
-
-        self.conv22d = nn.ConvTranspose2d(96, 32, kernel_size=3, padding=1)
-        self.bn22d = nn.BatchNorm2d(32)
-        self.do22d = nn.Dropout2d(p=0.2)
-        self.conv21d = nn.ConvTranspose2d(32, 16, kernel_size=3, padding=1)
-        self.bn21d = nn.BatchNorm2d(16)
-        self.do21d = nn.Dropout2d(p=0.2)
-
-        self.upconv1 = nn.ConvTranspose2d(16, 16, kernel_size=3, padding=1, stride=2, output_padding=1)
-
-        self.conv12d = nn.ConvTranspose2d(48, 16, kernel_size=3, padding=1)
-        self.bn12d = nn.BatchNorm2d(16)
-        self.do12d = nn.Dropout2d(p=0.2)
-        self.conv11d = nn.ConvTranspose2d(16, label_nbr, kernel_size=3, padding=1)
-
-        self.sm = nn.LogSoftmax(dim=1)
 
     def forward(self, input):
-        x1 = tensor_to_variable(input['vhr_pre'])
-        x2 = tensor_to_variable(input['vhr_post'])
-        
-        # Stage 1
-        x11 = self.do11(F.relu(self.bn11(self.conv11(x1))))
-        x12_1 = self.do12(F.relu(self.bn12(self.conv12(x11))))
-        x1p = F.max_pool2d(x12_1, kernel_size=2, stride=2)
+        post = tensor_to_variable(input['vhr_post'])
+        pre = tensor_to_variable(input['vhr_pre'])
 
+        pre_x1 = self.inc(pre)
+        pre_x2 = self.down1(pre_x1)
+        pre_x3 = self.down2(pre_x2)
+        pre_x4 = self.down3(pre_x3)
+        pre_x5 = self.down4(pre_x4)
 
-        # Stage 2
-        x21 = self.do21(F.relu(self.bn21(self.conv21(x1p))))
-        x22_1 = self.do22(F.relu(self.bn22(self.conv22(x21))))
-        x2p = F.max_pool2d(x22_1, kernel_size=2, stride=2)
+        post_x1 = self.inc(post)
+        post_x2 = self.down1(post_x1)
+        post_x3 = self.down2(post_x2)
+        post_x4 = self.down3(post_x3)
+        post_x5 = self.down4(post_x4)
 
-        # Stage 3
-        x31 = self.do31(F.relu(self.bn31(self.conv31(x2p))))
-        x32 = self.do32(F.relu(self.bn32(self.conv32(x31))))
-        x33_1 = self.do33(F.relu(self.bn33(self.conv33(x32))))
-        x3p = F.max_pool2d(x33_1, kernel_size=2, stride=2)
+        x = self.up1(x5, torch.cat([pre_x4, post_x4], dim=1))
+        x = self.up2(x, torch.cat([pre_x3, post_x3], dim=1))
+        x = self.up3(x, torch.cat([pre_x2, post_x2], dim=1))
+        x = self.up4(x, torch.cat([pre_x1, post_x1], dim=1))
+        x = self.outc(x)
 
-        # Stage 4
-        x41 = self.do41(F.relu(self.bn41(self.conv41(x3p))))
-        x42 = self.do42(F.relu(self.bn42(self.conv42(x41))))
-        x43_1 = self.do43(F.relu(self.bn43(self.conv43(x42))))
-        x4p = F.max_pool2d(x43_1, kernel_size=2, stride=2)
-
-
-        ####################################################
-        # Stage 1
-        x11 = self.do11(F.relu(self.bn11(self.conv11(x2))))
-        x12_2 = self.do12(F.relu(self.bn12(self.conv12(x11))))
-        x1p = F.max_pool2d(x12_2, kernel_size=2, stride=2)
-
-        # Stage 2
-        x21 = self.do21(F.relu(self.bn21(self.conv21(x1p))))
-        x22_2 = self.do22(F.relu(self.bn22(self.conv22(x21))))
-        x2p = F.max_pool2d(x22_2, kernel_size=2, stride=2)
-
-        # Stage 3
-        x31 = self.do31(F.relu(self.bn31(self.conv31(x2p))))
-        x32 = self.do32(F.relu(self.bn32(self.conv32(x31))))
-        x33_2 = self.do33(F.relu(self.bn33(self.conv33(x32))))
-        x3p = F.max_pool2d(x33_2, kernel_size=2, stride=2)
-
-        # Stage 4
-        x41 = self.do41(F.relu(self.bn41(self.conv41(x3p))))
-        x42 = self.do42(F.relu(self.bn42(self.conv42(x41))))
-        x43_2 = self.do43(F.relu(self.bn43(self.conv43(x42))))
-        x4p = F.max_pool2d(x43_2, kernel_size=2, stride=2)
-
-
-        ####################################################
-        # Stage 4d
-        x4d = self.upconv4(x4p)
-        pad4 = ReplicationPad2d((0, x43_1.size(3) - x4d.size(3), 0, x43_1.size(2) - x4d.size(2)))
-        x4d = torch.cat((pad4(x4d), x43_1, x43_2), 1)
-        x43d = self.do43d(F.relu(self.bn43d(self.conv43d(x4d))))
-        x42d = self.do42d(F.relu(self.bn42d(self.conv42d(x43d))))
-        x41d = self.do41d(F.relu(self.bn41d(self.conv41d(x42d))))
-
-        # Stage 3d
-        x3d = self.upconv3(x41d)
-        pad3 = ReplicationPad2d((0, x33_1.size(3) - x3d.size(3), 0, x33_1.size(2) - x3d.size(2)))
-        x3d = torch.cat((pad3(x3d), x33_1, x33_2), 1)
-        x33d = self.do33d(F.relu(self.bn33d(self.conv33d(x3d))))
-        x32d = self.do32d(F.relu(self.bn32d(self.conv32d(x33d))))
-        x31d = self.do31d(F.relu(self.bn31d(self.conv31d(x32d))))
-
-        # Stage 2d
-        x2d = self.upconv2(x31d)
-        pad2 = ReplicationPad2d((0, x22_1.size(3) - x2d.size(3), 0, x22_1.size(2) - x2d.size(2)))
-        x2d = torch.cat((pad2(x2d), x22_1, x22_2), 1)
-        x22d = self.do22d(F.relu(self.bn22d(self.conv22d(x2d))))
-        x21d = self.do21d(F.relu(self.bn21d(self.conv21d(x22d))))
-
-        # Stage 1d
-        x1d = self.upconv1(x21d)
-        pad1 = ReplicationPad2d((0, x12_1.size(3) - x1d.size(3), 0, x12_1.size(2) - x1d.size(2)))
-        x1d = torch.cat((pad1(x1d), x12_1, x12_2), 1)
-        x12d = self.do12d(F.relu(self.bn12d(self.conv12d(x1d))))
-        x11d = self.conv11d(x12d)
-
-        return self.sm(x11d)
+    	class_idx = x.size().index(self.n_classes)
+        return F.log_softmax(x, dim=class_idx)
 
 
 class SiamUnet_diff(nn.Module):
-    """SiamUnet_diff segmentation network."""
+    """SiamUnet_conc segmentation network."""
 
-    def __init__(self, input_nbr, label_nbr):
+    def __init__(self, n_channels, n_classes):
         super(SiamUnet_diff, self).__init__()
+	
+        self.n_classes = n_classes
+        self.inc = inconv(n_channels, 64)
+        self.down1 = down(64, 128)
+        self.down2 = down(128, 256)
+        self.down3 = down(256, 512)
+        self.down4 = down(512, 512)
+        self.up1 = up_skip(1024, 256)
+        self.up2 = up_skip(512, 128)
+        self.up3 = up_skip(256, 64)
+        self.up4 = up_skip(128, 64)
+        self.outc = outconv(64, n_classes)
 
-        self.input_nbr = input_nbr
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
 
-        self.conv11 = nn.Conv2d(input_nbr, 16, kernel_size=3, padding=1)
-        self.bn11 = nn.BatchNorm2d(16)
-        self.do11 = nn.Dropout2d(p=0.2)
-        self.conv12 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
-        self.bn12 = nn.BatchNorm2d(16)
-        self.do12 = nn.Dropout2d(p=0.2)
-
-        self.conv21 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.bn21 = nn.BatchNorm2d(32)
-        self.do21 = nn.Dropout2d(p=0.2)
-        self.conv22 = nn.Conv2d(32, 32, kernel_size=3, padding=1)
-        self.bn22 = nn.BatchNorm2d(32)
-        self.do22 = nn.Dropout2d(p=0.2)
-
-        self.conv31 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
-        self.bn31 = nn.BatchNorm2d(64)
-        self.do31 = nn.Dropout2d(p=0.2)
-        self.conv32 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.bn32 = nn.BatchNorm2d(64)
-        self.do32 = nn.Dropout2d(p=0.2)
-        self.conv33 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.bn33 = nn.BatchNorm2d(64)
-        self.do33 = nn.Dropout2d(p=0.2)
-
-        self.conv41 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        self.bn41 = nn.BatchNorm2d(128)
-        self.do41 = nn.Dropout2d(p=0.2)
-        self.conv42 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.bn42 = nn.BatchNorm2d(128)
-        self.do42 = nn.Dropout2d(p=0.2)
-        self.conv43 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
-        self.bn43 = nn.BatchNorm2d(128)
-        self.do43 = nn.Dropout2d(p=0.2)
-
-        self.upconv4 = nn.ConvTranspose2d(128, 128, kernel_size=3, padding=1, stride=2, output_padding=1)
-
-        self.conv43d = nn.ConvTranspose2d(256, 128, kernel_size=3, padding=1)
-        self.bn43d = nn.BatchNorm2d(128)
-        self.do43d = nn.Dropout2d(p=0.2)
-        self.conv42d = nn.ConvTranspose2d(128, 128, kernel_size=3, padding=1)
-        self.bn42d = nn.BatchNorm2d(128)
-        self.do42d = nn.Dropout2d(p=0.2)
-        self.conv41d = nn.ConvTranspose2d(128, 64, kernel_size=3, padding=1)
-        self.bn41d = nn.BatchNorm2d(64)
-        self.do41d = nn.Dropout2d(p=0.2)
-
-        self.upconv3 = nn.ConvTranspose2d(64, 64, kernel_size=3, padding=1, stride=2, output_padding=1)
-
-        self.conv33d = nn.ConvTranspose2d(128, 64, kernel_size=3, padding=1)
-        self.bn33d = nn.BatchNorm2d(64)
-        self.do33d = nn.Dropout2d(p=0.2)
-        self.conv32d = nn.ConvTranspose2d(64, 64, kernel_size=3, padding=1)
-        self.bn32d = nn.BatchNorm2d(64)
-        self.do32d = nn.Dropout2d(p=0.2)
-        self.conv31d = nn.ConvTranspose2d(64, 32, kernel_size=3, padding=1)
-        self.bn31d = nn.BatchNorm2d(32)
-        self.do31d = nn.Dropout2d(p=0.2)
-
-        self.upconv2 = nn.ConvTranspose2d(32, 32, kernel_size=3, padding=1, stride=2, output_padding=1)
-
-        self.conv22d = nn.ConvTranspose2d(64, 32, kernel_size=3, padding=1)
-        self.bn22d = nn.BatchNorm2d(32)
-        self.do22d = nn.Dropout2d(p=0.2)
-        self.conv21d = nn.ConvTranspose2d(32, 16, kernel_size=3, padding=1)
-        self.bn21d = nn.BatchNorm2d(16)
-        self.do21d = nn.Dropout2d(p=0.2)
-
-        self.upconv1 = nn.ConvTranspose2d(16, 16, kernel_size=3, padding=1, stride=2, output_padding=1)
-
-        self.conv12d = nn.ConvTranspose2d(32, 16, kernel_size=3, padding=1)
-        self.bn12d = nn.BatchNorm2d(16)
-        self.do12d = nn.Dropout2d(p=0.2)
-        self.conv11d = nn.ConvTranspose2d(16, label_nbr, kernel_size=3, padding=1)
-
-        self.sm = nn.LogSoftmax(dim=1)
 
     def forward(self, input):
-        x1 = tensor_to_variable(input['vhr_pre'])
-        x2 = tensor_to_variable(input['vhr_post'])
+        post = tensor_to_variable(input['vhr_post'])
+        pre = tensor_to_variable(input['vhr_pre'])
 
-        """Forward method."""
-        # Stage 1
-        x11 = self.do11(F.relu(self.bn11(self.conv11(x1))))
-        x12_1 = self.do12(F.relu(self.bn12(self.conv12(x11))))
-        x1p = F.max_pool2d(x12_1, kernel_size=2, stride=2)
+        pre_x1 = self.inc(pre)
+        pre_x2 = self.down1(pre_x1)
+        pre_x3 = self.down2(pre_x2)
+        pre_x4 = self.down3(pre_x3)
+        pre_x5 = self.down4(pre_x4)
 
+        post_x1 = self.inc(post)
+        post_x2 = self.down1(post_x1)
+        post_x3 = self.down2(post_x2)
+        post_x4 = self.down3(post_x3)
+        post_x5 = self.down4(post_x4)
 
-        # Stage 2
-        x21 = self.do21(F.relu(self.bn21(self.conv21(x1p))))
-        x22_1 = self.do22(F.relu(self.bn22(self.conv22(x21))))
-        x2p = F.max_pool2d(x22_1, kernel_size=2, stride=2)
+        x = self.up1(x5, post_x4 - pre_x4)
+        x = self.up2(x, post_x3 - pre_x3)
+        x = self.up3(x, post_x2 - pre_x2)
+        x = self.up4(x, post_x1 - pre_x1)
+        x = self.outc(x)
 
-        # Stage 3
-        x31 = self.do31(F.relu(self.bn31(self.conv31(x2p))))
-        x32 = self.do32(F.relu(self.bn32(self.conv32(x31))))
-        x33_1 = self.do33(F.relu(self.bn33(self.conv33(x32))))
-        x3p = F.max_pool2d(x33_1, kernel_size=2, stride=2)
-
-        # Stage 4
-        x41 = self.do41(F.relu(self.bn41(self.conv41(x3p))))
-        x42 = self.do42(F.relu(self.bn42(self.conv42(x41))))
-        x43_1 = self.do43(F.relu(self.bn43(self.conv43(x42))))
-        x4p = F.max_pool2d(x43_1, kernel_size=2, stride=2)
-
-        ####################################################
-        # Stage 1
-        x11 = self.do11(F.relu(self.bn11(self.conv11(x2))))
-        x12_2 = self.do12(F.relu(self.bn12(self.conv12(x11))))
-        x1p = F.max_pool2d(x12_2, kernel_size=2, stride=2)
-
-
-        # Stage 2
-        x21 = self.do21(F.relu(self.bn21(self.conv21(x1p))))
-        x22_2 = self.do22(F.relu(self.bn22(self.conv22(x21))))
-        x2p = F.max_pool2d(x22_2, kernel_size=2, stride=2)
-
-        # Stage 3
-        x31 = self.do31(F.relu(self.bn31(self.conv31(x2p))))
-        x32 = self.do32(F.relu(self.bn32(self.conv32(x31))))
-        x33_2 = self.do33(F.relu(self.bn33(self.conv33(x32))))
-        x3p = F.max_pool2d(x33_2, kernel_size=2, stride=2)
-
-        # Stage 4
-        x41 = self.do41(F.relu(self.bn41(self.conv41(x3p))))
-        x42 = self.do42(F.relu(self.bn42(self.conv42(x41))))
-        x43_2 = self.do43(F.relu(self.bn43(self.conv43(x42))))
-        x4p = F.max_pool2d(x43_2, kernel_size=2, stride=2)
-
-
-
-        # Stage 4d
-        x4d = self.upconv4(x4p)
-        pad4 = ReplicationPad2d((0, x43_1.size(3) - x4d.size(3), 0, x43_1.size(2) - x4d.size(2)))
-        x4d = torch.cat((pad4(x4d), torch.abs(x43_1 - x43_2)), 1)
-        x43d = self.do43d(F.relu(self.bn43d(self.conv43d(x4d))))
-        x42d = self.do42d(F.relu(self.bn42d(self.conv42d(x43d))))
-        x41d = self.do41d(F.relu(self.bn41d(self.conv41d(x42d))))
-
-        # Stage 3d
-        x3d = self.upconv3(x41d)
-        pad3 = ReplicationPad2d((0, x33_1.size(3) - x3d.size(3), 0, x33_1.size(2) - x3d.size(2)))
-        x3d = torch.cat((pad3(x3d), torch.abs(x33_1 - x33_2)), 1)
-        x33d = self.do33d(F.relu(self.bn33d(self.conv33d(x3d))))
-        x32d = self.do32d(F.relu(self.bn32d(self.conv32d(x33d))))
-        x31d = self.do31d(F.relu(self.bn31d(self.conv31d(x32d))))
-
-        # Stage 2d
-        x2d = self.upconv2(x31d)
-        pad2 = ReplicationPad2d((0, x22_1.size(3) - x2d.size(3), 0, x22_1.size(2) - x2d.size(2)))
-        x2d = torch.cat((pad2(x2d), torch.abs(x22_1 - x22_2)), 1)
-        x22d = self.do22d(F.relu(self.bn22d(self.conv22d(x2d))))
-        x21d = self.do21d(F.relu(self.bn21d(self.conv21d(x22d))))
-
-        # Stage 1d
-        x1d = self.upconv1(x21d)
-        pad1 = ReplicationPad2d((0, x12_1.size(3) - x1d.size(3), 0, x12_1.size(2) - x1d.size(2)))
-        x1d = torch.cat((pad1(x1d), torch.abs(x12_1 - x12_2)), 1)
-        x12d = self.do12d(F.relu(self.bn12d(self.conv12d(x1d))))
-        x11d = self.conv11d(x12d)
-
-        return self.sm(x11d)
+    	class_idx = x.size().index(self.n_classes)
+        return F.log_softmax(x, dim=class_idx)
 
     
